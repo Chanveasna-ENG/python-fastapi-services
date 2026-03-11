2f0aca2383f2f8daf406f704dc8567cc5d560030
Chanveasna-ENG <2023488eng@aupp.edu.kh>
2026-03-12 00:30:35 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import requests # to talk to the Kokoro container
+import os
+
+app = FastAPI(title="Python-FastAPI-Services")
+
+KOKORO_SERVICE_URL = os.getenv("KOKORO_URL")
+
+@app.get("/")
+def read_root():
+    return {"status": "online", "message": "Services are running!"}
+
+# Example 1: A wrapper for Kokoro
+@app.post("/read")
+def say_hello(text: str, voice: str="bf_emma"):
+
+    response = requests.post(KOKORO_SERVICE_URL, json={
+        "model": "kokoro",
+        "input": text,
+        "voice": voice
+    })
+    return Response(content=kokoro_res.content, media_type="audio/mpeg")
