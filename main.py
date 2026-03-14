f19faa2dfae662aefac48569a0f33846e1d5bc4d
ENG Chanveasna <2023488eng@aupp.edu.kh>
2026-03-14 16:52:41 +0700

    fix: force fastapi to look for body and not the query.

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 # main.py
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 import requests
 import os
 
 app = FastAPI(title="Python-FastAPI-Services")
 
-KOKORO_SERVICE_URL = os.getenv("KOKORO_URL")
+class SpeechRequest(BaseModel):
+    text: str
+    voice: str = "bf_emma"
+
 
 @app.get("/")
 def read_root():
     return {"status": "online", "message": "Services are running!!!"}
 
+
 # A wrapper for Kokoro
 @app.post("/read")
-def say_hello(text: str, voice: str="bf_emma"):
-
+def generate_speech(request: SpeechRequest): # Use the model here
+    KOKORO_SERVICE_URL = os.getenv("KOKORO_URL")
+    
+    # Access data via request.text and request.voice
     response = requests.post(KOKORO_SERVICE_URL, json={
         "model": "kokoro",
-        "input": text,
-        "voice": voice
+        "input": request.text,
+        "voice": request.voice
     })
-    return Response(content=kokoro_res.content, media_type="audio/mpeg")
+    
+    return Response(content=response.content, media_type="audio/mpeg")
